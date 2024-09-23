b7da5b2db1f2750cc3caffe77474cebf62de3e33
Juaan26 <Juaan26g@gmail.com>
2024-09-23 05:36:44 +0200

    +breakpoints +projects es responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			'xs': '320px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280',
+			'2xl': '1536px',
+			
+		  },
 		extend: {},
 	},
 	plugins: [],
